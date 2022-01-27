97d8ce58a3a03eeea15d94c974a4c0c62acaa117
Mark Calvert <markcalvert84@gmail.com>
2022-01-28 09:02:03 +1300

    Override harvest gather_stage to implement custom logic Gather stage will now create harvest objects on each api call instead of at the end which causes memory issues storing all of the retrevied datasets in memory

--- ckanext/data_qld_harvester/geoscience_ckan_harvester.py
+++ ckanext/data_qld_harvester/geoscience_ckan_harvester.py
@@ -8,6 +8,10 @@ from ckanext.harvest.model import HarvestObjectExtra as HOExtra
 
 import six
 
+
+import datetime
+import urllib
+
 log = logging.getLogger(__name__)
 
 
@@ -147,39 +151,213 @@ class GeoScienceCKANHarvester(CKANHarvester):
 
         self._set_config(harvest_job.source.config)
 
-        guids_in_source = super(GeoScienceCKANHarvester, self).gather_stage(harvest_job)
-
-        # Get the previous guids for this source
-        query = model.Session.query(HarvestObject.guid, HarvestObject.package_id).\
-            filter(HarvestObject.current == True).\
-            filter(HarvestObject.harvest_source_id == harvest_job.source.id)
-
-        guid_to_package_id = {guid: package_id for guid, package_id in query}
-
-        guids_in_db = set(guid_to_package_id.keys())
-
-        # Check datasets that need to be deleted
-        guids_to_delete = set(guids_in_db) - set(guids_in_source)
-        for guid in guids_to_delete:
-            # Delete package
-            package_id = guid_to_package_id[guid]
-            context = {'model': model, 'session': model.Session,
-                       'user': self._get_user_name()}
-            try:
-                data_dict = {"id": package_id, "deletion_reason": self.config.get('deletion_reason')}
-                toolkit.get_action('package_delete')(context, data_dict)
-                log.info('Deleted package {0} with guid {1}'.format(package_id, guid))
-
-                obj = HarvestObject(guid=guid, job=harvest_job,
-                                    package_id=guid_to_package_id[guid],
-                                    extras=[HOExtra(key='status', value='delete')])
-
-                model.Session.query(HarvestObject).\
-                    filter_by(guid=guid).\
-                    update({'current': False}, False)
-                obj.save()
-            except Exception as e:
-                # TODO: What should we do here?
-                log.error('Deleting package {0} with guid {1} failed'.format(package_id, guid))
+        guids_in_source = self._gather_stage(harvest_job)
 
         return guids_in_source
+
+    def _gather_stage(self, harvest_job):
+        log.debug('In CKANHarvester gather_stage (%s)',
+                  harvest_job.source.url)
+        toolkit.requires_ckan_version(min_version='2.0')
+        get_all_packages = True
+
+        self._set_config(harvest_job.source.config)
+
+        # Get source URL
+        remote_ckan_base_url = harvest_job.source.url.rstrip('/')
+
+        # Filter in/out datasets from particular organizations
+        fq_terms = []
+        org_filter_include = self.config.get('organizations_filter_include', [])
+        org_filter_exclude = self.config.get('organizations_filter_exclude', [])
+        if org_filter_include:
+            fq_terms.append(' OR '.join(
+                'organization:%s' % org_name for org_name in org_filter_include))
+        elif org_filter_exclude:
+            fq_terms.extend(
+                '-organization:%s' % org_name for org_name in org_filter_exclude)
+
+        groups_filter_include = self.config.get('groups_filter_include', [])
+        groups_filter_exclude = self.config.get('groups_filter_exclude', [])
+        if groups_filter_include:
+            fq_terms.append(' OR '.join(
+                'groups:%s' % group_name for group_name in groups_filter_include))
+        elif groups_filter_exclude:
+            fq_terms.extend(
+                '-groups:%s' % group_name for group_name in groups_filter_exclude)
+
+        # Ideally we can request from the remote CKAN only those datasets
+        # modified since the last completely successful harvest.
+        last_error_free_job = self.last_error_free_job(harvest_job)
+        log.debug('Last error-free job: %r', last_error_free_job)
+        if (last_error_free_job and
+                not self.config.get('force_all', False)):
+            get_all_packages = False
+
+            # Request only the datasets modified since
+            last_time = last_error_free_job.gather_started
+            # Note: SOLR works in UTC, and gather_started is also UTC, so
+            # this should work as long as local and remote clocks are
+            # relatively accurate. Going back a little earlier, just in case.
+            get_changes_since = \
+                (last_time - datetime.timedelta(hours=1)).isoformat()
+            log.info('Searching for datasets modified since: %s UTC',
+                     get_changes_since)
+
+            fq_since_last_time = 'metadata_modified:[{since}Z TO *]' \
+                .format(since=get_changes_since)
+
+            try:
+                object_ids = self._search_for_datasets(
+                    harvest_job,
+                    remote_ckan_base_url,
+                    fq_terms + [fq_since_last_time])
+            except SearchError as e:
+                log.info('Searching for datasets changed since last time '
+                         'gave an error: %s', e)
+                get_all_packages = True
+
+            if not get_all_packages and not object_ids:
+                log.info('No datasets have been updated on the remote '
+                         'CKAN instance since the last harvest job %s',
+                         last_time)
+                return []
+
+        # Fall-back option - request all the datasets from the remote CKAN
+        if get_all_packages:
+            # Request all remote packages
+            try:
+                object_ids = self._search_for_datasets(harvest_job,
+                                                       remote_ckan_base_url,
+                                                       fq_terms)
+            except SearchError as e:
+                log.info('Searching for all datasets gave an error: %s', e)
+                self._save_gather_error(
+                    'Unable to search remote CKAN for datasets:%s url:%s'
+                    'terms:%s' % (e, remote_ckan_base_url, fq_terms),
+                    harvest_job)
+                return None
+        if not object_ids:
+            self._save_gather_error(
+                'No datasets found at CKAN: %s' % remote_ckan_base_url,
+                harvest_job)
+            return []
+
+        return object_ids
+
+    def _search_for_datasets(self, harvest_job, remote_ckan_base_url, fq_terms=None):
+        '''Does a dataset search on a remote CKAN and returns the results.
+
+        Deals with paging to return all the results, not just the first page.
+        '''
+        base_search_url = remote_ckan_base_url + self._get_search_api_offset()
+        params = {'rows': '1000', 'start': '0'}
+        # There is the worry that datasets will be changed whilst we are paging
+        # through them.
+        # * In SOLR 4.7 there is a cursor, but not using that yet
+        #   because few CKANs are running that version yet.
+        # * However we sort, then new names added or removed before the current
+        #   page would cause existing names on the next page to be missed or
+        #   double counted.
+        # * Another approach might be to sort by metadata_modified and always
+        #   ask for changes since (and including) the date of the last item of
+        #   the day before. However if the entire page is of the exact same
+        #   time, then you end up in an infinite loop asking for the same page.
+        # * We choose a balanced approach of sorting by ID, which means
+        #   datasets are only missed if some are removed, which is far less
+        #   likely than any being added. If some are missed then it is assumed
+        #   they will harvested the next time anyway. When datasets are added,
+        #   we are at risk of seeing datasets twice in the paging, so we detect
+        #   and remove any duplicates.
+        params['sort'] = 'id asc'
+        if fq_terms:
+            params['fq'] = ' '.join(fq_terms)
+
+        object_ids = []
+        pkg_ids = set()
+        previous_content = None
+        while True:
+            url = base_search_url + '?' + urllib.urlencode(params)
+            log.debug('Searching for CKAN datasets: %s', url)
+            try:
+                content = self._get_content(url)
+            except ContentFetchError as e:
+                raise SearchError(
+                    'Error sending request to search remote '
+                    'CKAN instance %s using URL %r. Error: %s' %
+                    (remote_ckan_base_url, url, e))
+
+            if previous_content and content == previous_content:
+                raise SearchError('The paging doesn\'t seem to work. URL: %s' %
+                                  url)
+            try:
+                response_dict = json.loads(content)
+            except ValueError:
+                raise SearchError('Response from remote CKAN was not JSON: %r'
+                                  % content)
+            try:
+                pkg_dicts_page = response_dict.get('result', {}).get('results',
+                                                                     [])
+            except ValueError:
+                raise SearchError('Response JSON did not contain '
+                                  'result/results: %r' % response_dict)
+
+            # Weed out any datasets found on previous pages (should datasets be
+            # changing while we page)
+            ids_in_page = set(p['id'] for p in pkg_dicts_page)
+            duplicate_ids = ids_in_page & pkg_ids
+            if duplicate_ids:
+                pkg_dicts_page = [p for p in pkg_dicts_page
+                                  if p['id'] not in duplicate_ids]
+            pkg_ids |= ids_in_page
+
+            # pkg_dicts.extend(pkg_dicts_page)
+            object_ids.extend(self._create_harvest_objects(pkg_dicts_page, harvest_job))
+
+            if len(pkg_dicts_page) == 0:
+                break
+
+            params['start'] = str(int(params['start']) + int(params['rows']))
+
+        return object_ids
+
+    def _create_harvest_objects(self, pkg_dicts, harvest_job):
+        object_ids = []
+        try:
+            package_ids = set()
+            for pkg_dict in pkg_dicts:
+                if pkg_dict['id'] in package_ids:
+                    log.info('Discarding duplicate dataset %s - probably due '
+                             'to datasets being changed at the same time as '
+                             'when the harvester was paging through',
+                             pkg_dict['id'])
+                    continue
+                package_ids.add(pkg_dict['id'])
+
+                log.debug('Creating HarvestObject for %s %s',
+                          pkg_dict['name'], pkg_dict['id'])
+                obj = HarvestObject(guid=pkg_dict['id'],
+                                    job=harvest_job,
+                                    content=json.dumps(pkg_dict))
+                obj.save()
+                object_ids.append(obj.id)
+
+            return object_ids
+        except Exception as e:
+            self._save_gather_error('%r' % e.message, harvest_job)
+
+
+class ContentFetchError(Exception):
+    pass
+
+
+class ContentNotFoundError(ContentFetchError):
+    pass
+
+
+class RemoteResourceError(Exception):
+    pass
+
+
+class SearchError(Exception):
+    pass
