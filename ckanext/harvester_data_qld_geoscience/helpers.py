bafe825bb0649a2badc8670b348dea155f389409
william dutton <will.dutt@gmail.com>
2022-04-08 14:34:07 +1000

    use dict lookup instead of . style

--- ckanext/harvester_data_qld_geoscience/helpers.py
+++ ckanext/harvester_data_qld_geoscience/helpers.py
@@ -4,10 +4,10 @@
 # {% set label = label_function(item) if label_function else item.display_name %}
 # {% set label_truncated = h.truncate(label, 22) if not label_function else label %}
 def custom_label_function(facet_item):
-    if facet_item.name == 'dataset':
+    if facet_item['name'] == 'dataset':
         label = 'data.qld.gov.au'
-    elif facet_item.name == 'geoscience':
+    elif facet_item['name'] == 'geoscience':
         label = 'geoscience.data.qld.gov.au'
     else:
-        label = facet_item.display_name
+        label = facet_item['display_name']
     return label
