8e967b7ec316982b0e6f25ac2ec2a310cf25e9e4
william dutton <will.dutt@gmail.com>
2022-04-08 15:21:20 +1000

    pass in field being referenced so we don't change other facet items that use the same 'name'

--- ckanext/harvester_data_qld_geoscience/helpers.py
+++ ckanext/harvester_data_qld_geoscience/helpers.py
@@ -2,15 +2,14 @@
 
 # based on facet_list.html functionality
 # {% set label = label_function(item) if label_function else item.display_name %}
-# {% set label_truncated = h.truncate(label, 22) if not label_function else label %}
-def custom_label_function(facet_item):
-    if facet_item['name'] == 'dataset':
-        label = 'data.qld.gov.au'
-    elif facet_item['name'] == 'geoscience':
-        label = 'geoscience.data.qld.gov.au'
-    else:
-        label = facet_item['display_name']
-    return label
+def custom_label_function(facet_field, facet_item):
+    if facet_field == 'dataset_type':
+        if facet_item['name'] == 'dataset':
+            return 'data.qld.gov.au'
+        elif facet_item['name'] == 'geoscience':
+            return 'geoscience.data.qld.gov.au'
+
+    return facet_item['display_name']
 
 # based on
 # @core_helper
