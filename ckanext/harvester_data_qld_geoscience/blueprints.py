32ff7ea32b590e2d7ca059657fbbe1535001d5d8
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-13 10:48:23 +1000

    [QOL-8754] add Pylons and Flask support for custom dataset type
    - routing should be the same as core datasets

---
+++ ckanext/harvester_data_qld_geoscience/blueprints.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+from flask import Blueprint
+
+from ckan.views.package import read
+
+blueprint = Blueprint(
+    u'geoscience',
+    __name__,
+    url_prefix=u'/dataset/',
+    url_defaults={u'package_type': u'dataset'}
+)
+
+blueprint.add_url_rule(u'/<id>', view_func=read)
+
+
+def get_blueprints():
+    return [blueprint]
