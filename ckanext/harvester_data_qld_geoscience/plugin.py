a1a715371048d182ec02db16636208ca39bf1f9d
william dutton <will.dutt@gmail.com>
2022-04-13 16:24:40 +1000

    Revert "align to harvester plugin limit"
    This reverts commit 2c991163424dc8fa18694888d14fa8bd157d101b.

--- ckanext/harvester_data_qld_geoscience/plugin.py
+++ ckanext/harvester_data_qld_geoscience/plugin.py
@@ -272,7 +272,7 @@ class GeoScienceCKANHarvester(CKANHarvester):
         Deals with paging to return all the results, not just the first page.
         '''
         base_search_url = remote_ckan_base_url + self._get_search_api_offset()
-        params = {'rows': '100', 'start': '0'}
+        params = {'rows': '1000', 'start': '0'}
         # There is the worry that datasets will be changed whilst we are paging
         # through them.
         # * In SOLR 4.7 there is a cursor, but not using that yet
