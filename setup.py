77527365a3937c2e627caf20961d11ab89ade7ff
william dutton <will.dutt@gmail.com>
2023-02-08 12:44:58 +1000

    increment setup.py to tagged version 0.0.8 fix harvest import QOLSVC-740

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    # https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers
+    version='0.0.8',
 
     description='Custom harvesters for Data.Qld',
     long_description=long_description,
